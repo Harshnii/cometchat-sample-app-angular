5a4662758df121682dce07df2723d92e8f911918
Harshnii <150833728+Harshnii@users.noreply.github.com>
2024-05-05 21:04:05 +0530

    Update AppConstants.ts

--- src/AppConstants.ts
+++ src/AppConstants.ts
@@ -1,5 +1,5 @@
 export const AppConstants = {
-  APP_ID: "APP_ID",
-  REGION: "REGION",
-  AUTH_KEY: "AUTH_KEY",
+  APP_ID: "2572068299f86a4a",
+  REGION: "in",
+  AUTH_KEY: "e2170a10f15dd894fe3596e31182a6a0a08f2b7f",
 };
